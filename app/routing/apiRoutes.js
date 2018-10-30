69c795d1700af24d5a4fe46c52e1925452770072
Kora25 <sarekabelnavis@gmail.com>
2018-10-30 09:34:23 -0400

    Updating folder and file structure

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+// 4. Your `apiRoutes.js` file should contain two routes:
+
+        //    * A GET route with the url `/api/employees`. This will be used to display a JSON of all possible employees.
+        //    * A POST routes `/api/employees`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+
+
+
+        
